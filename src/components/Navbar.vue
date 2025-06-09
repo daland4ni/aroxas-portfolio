01bee6159a4f44aae51441180334cf96a772ba27
Aeron Roxas <aeronroxas01@gmail.com>
2025-06-09 12:08:38 +0800

    Initialized files

---
+++ src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Admin Dashboard</a>
+  </nav>
+</template>
+<script>
+export default { name: 'Navbar' };
+</script>
